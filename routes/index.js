90402d8bd0fc7273e9ce52a2ecca2799ef60735c
Arisandi <arisandisatria541@gmail.com>
2023-05-26 01:36:52 +0700

    Fixed route /

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const userRouter = require("./userRoute");
 
 const router = require("express").Router();
 
-router.get("/", getUsers.getAll);
+router.get("/", (req, res) => {
+  res.send("<h1>Selamat Datang!</h1>");
+});
 
 router.use(userRouter);
 router.use(todoRouter);
